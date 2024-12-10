fc8997795cac044e553bc2475268c22f409bc51e
vamshi2903 <vamshi290303@gmail.com>
2024-12-10 09:14:21 +0530

    done

---
+++ switch.js
@@ -0,0 +1,52 @@
+// let day=5;
+// switch(day){
+//     case 1 :
+//         console.log(`hai`);
+//         break;
+//     case 2 :
+//         console.log(`hello`);
+//         break;
+//     default :
+//         console.log(`Bye`);
+// }
+
+
+
+// let username = `         Vamshai           `;
+// console.log(username.charAt(2));
+// console.log(username.indexOf(`a`));
+// console.log(username.lastIndexOf(`a`));
+// console.log(username.length);
+// console.log(username.trim());
+// console.log(username.toUpperCase());
+// console.log(username.repeat(3));
+// console.log(username.startsWith(` `));
+
+
+
+// let phonenumber= `123-456-7890`;
+// console.log(phonenumber.replaceAll(`-`,","));
+// console.log(phonenumber.padStart(15,"!")); //increases the length of the string
+
+
+
+// const fullname=`Vamshi Yarramalla`;
+// let firstname = fullname.slice(0,6);
+// console.log(firstname);
+// let lastname = fullname.slice(7); 
+// console.log(lastname);
+
+// let firstname = fullname.slice(0,fullname.indexOf(" "));
+// console.log(firstname);
+// let lastname = fullname.slice(fullname.indexOf(" ")+1);
+// console.log(lastname);
+
+// const email = `vamshi290303@gmail.com`;
+// let username = email.slice(0,email.indexOf(`@`));
+// console.log(username);
+
+ 
+
+
+
+
